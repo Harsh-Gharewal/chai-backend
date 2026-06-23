23b5129d1edf71b5dd1c26b17bbce59dc7406eac
Harsh Gharewal <0801me231043@sgsits.ac.in>
2026-06-23 20:10:11 +0530

    User setup

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -62,12 +62,12 @@ userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) {
         return next();
     }
-    this.password = bcrypt.hashSync(this.password, 10);
+    this.password = await bcrypt.hash(this.password, 10);
     next();
 });
 
-userSchema.methods.comparePassword = async function (password) {
-    return await bcrypt.compareSync(password, this.password);
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
 }
 
 userSchema.methods.generateAccessToken = function () {
